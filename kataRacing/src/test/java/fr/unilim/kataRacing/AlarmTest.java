d6912dd55d23c63106f05a7061e8da59b82958cc
Lara <lv.laravidoni@gmail.com>
2018-11-12 16:35:50 +0100

    test alarmStillOn

--- kataRacing/src/test/java/fr/unilim/kataRacing/AlarmTest.java
+++ kataRacing/src/test/java/fr/unilim/kataRacing/AlarmTest.java
@@ -43,4 +43,18 @@ public class AlarmTest{
 		alarm.check();
 		assertEquals(alarm.isAlarmOn(), false);
 	}
+	
+	@Test
+	public void isAlarmStillOn() {
+		Sensor sensor = mock(Sensor.class);
+		when(sensor.popNextPressurePsiValue()).thenReturn(16.0);
+		
+		Alarm alarm = new Alarm(sensor);
+		alarm.check();
+		assertEquals(alarm.isAlarmOn(), true);
+		
+		when(sensor.popNextPressurePsiValue()).thenReturn(18.0);
+		alarm.check();
+		assertEquals(alarm.isAlarmOn(), true);
+	}
 }
