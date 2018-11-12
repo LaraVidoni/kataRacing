e624081ca9e961c50df053864f02b2341cecf14a
Lara <lv.laravidoni@gmail.com>
2018-11-12 16:30:03 +0100

    1er test

--- kataRacing/src/main/java/fr/unilim/kataRacing/Alarm.java
+++ kataRacing/src/main/java/fr/unilim/kataRacing/Alarm.java
@@ -9,6 +9,9 @@ public class Alarm
 
     boolean alarmOn = false;
 
+    public Alarm(Sensor sensor) {
+    	this.sensor = sensor;
+    }
     public void check()
     {
         double psiPressureValue = sensor.popNextPressurePsiValue();
--- kataRacing/src/test/java/fr/unilim/kataRacing/AlarmTest.java
+++ kataRacing/src/test/java/fr/unilim/kataRacing/AlarmTest.java
@@ -1,7 +1,23 @@
 package fr.unilim.kataRacing;
 
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 import junit.framework.TestCase;
 
-public class AlarmTest extends TestCase {
+import static org.mockito.Mockito.mock; 
+import static org.mockito.Mockito.when;
 
+public class AlarmTest{
+
+	@Test
+	public void isAlarmOnWhenPressureLow() {
+		Sensor sensor = mock(Sensor.class);
+		when(sensor.popNextPressurePsiValue()).thenReturn(16.0);
+		
+		Alarm alarm = new Alarm(sensor);
+		
+		alarm.check();
+		assertEquals(alarm.isAlarmOn(), true);
+	}
 }
